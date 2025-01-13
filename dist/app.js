c9cc2f2c194fcff8e53ab55789f5fac21575714d
aldilukmn <aldilukman99@gmail.com>
2025-01-13 21:02:07 +0700

    add: new views ui

--- dist/app.js
+++ dist/app.js
@@ -6,8 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var dotenv_1 = __importDefault(require("dotenv"));
 var express_1 = __importDefault(require("express"));
 var router_1 = __importDefault(require("./router"));
+var express_ejs_layouts_1 = __importDefault(require("express-ejs-layouts"));
 dotenv_1.default.config();
 var app = (0, express_1.default)();
-app.use('/api', router_1.default);
+app.use(express_1.default.static('public'));
+app.set('view engine', 'ejs');
+app.use(express_ejs_layouts_1.default);
+app.use('/', router_1.default);
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- dist/router/index.js
+++ dist/router/index.js
@@ -7,10 +7,16 @@ var express_1 = __importDefault(require("express"));
 var controllers_1 = __importDefault(require("../controllers"));
 var uploadFile_1 = __importDefault(require("../utils/uploadFile"));
 var router = express_1.default.Router();
-router.get('/v1/gtk', controllers_1.default.listGtk);
-router.get('/v1/gtk/:id', controllers_1.default.getGtkById);
-router.post('/v1/gtk', uploadFile_1.default, controllers_1.default.createGtk);
-router.patch('/v1/gtk/:id', uploadFile_1.default, controllers_1.default.updateGtk);
-router.delete('/v1/gtk/:id', controllers_1.default.deleteGtkById);
+router.get('/', function (req, res) {
+    res.render('home', {
+        layout: 'layouts/main',
+        title: 'Home',
+    });
+});
+router.get('/api/v1/gtk', controllers_1.default.listGtk);
+router.get('/api/v1/gtk/:id', controllers_1.default.getGtkById);
+router.post('/api/v1/gtk', uploadFile_1.default, controllers_1.default.createGtk);
+router.patch('/api/v1/gtk/:id', uploadFile_1.default, controllers_1.default.updateGtk);
+router.delete('/api/v1/gtk/:id', controllers_1.default.deleteGtkById);
 exports.default = router;
 //# sourceMappingURL=index.js.map
