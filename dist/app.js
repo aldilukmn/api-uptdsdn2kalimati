d6baed63c977ff791e61fcd10450a46099edbce5
aldilukmn <aldilukman99@gmail.com>
2025-01-13 22:38:17 +0700

    add: cors module

--- dist/app.js
+++ dist/app.js
@@ -6,11 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var dotenv_1 = __importDefault(require("dotenv"));
 var express_1 = __importDefault(require("express"));
 var router_1 = __importDefault(require("./router"));
+var cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 var app = (0, express_1.default)();
 // app.use(express.static('public'));
 // app.set('view engine', 'ejs');
 // app.use(expressLayout);
+app.use((0, cors_1.default)());
 app.use('/', router_1.default);
 exports.default = app;
 //# sourceMappingURL=app.js.map
