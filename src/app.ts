95702b67758ade81ca4266313ddc91d975d03bc4
aldilukmn <aldilukman99@gmail.com>
2025-01-25 23:25:09 +0700

    fix: cors problem 13

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use(expressEjsLayouts);
 app.use(express.urlencoded({ extended: true })); //For Login body json, POST METHOD
 // app.use(express.json()); //For Login body json, POST METHOD
 app.use(cors({
-  origin: 'https://uptdsdn2kalimati.vercel.app', // Sesuaikan dengan origin frontend kamu
+  origin: ['https://uptdsdn2kalimati.vercel.app', 'http://localhost:5173'], // Sesuaikan dengan origin frontend kamu
   methods: ['GET', 'POST', 'PATCH', 'DELETE'], // Sesuaikan dengan method yang kamu perlukan
 }));
 
