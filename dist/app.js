bd8750eccd6ec3780838b94e41537e66518497c7
aldilukmn <aldilukman99@gmail.com>
2025-01-25 23:07:46 +0700

    fix: cors problem 10

--- dist/app.js
+++ dist/app.js
@@ -6,21 +6,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var router_1 = __importDefault(require("./router"));
 var cors_1 = __importDefault(require("cors"));
-var dotenv_1 = __importDefault(require("dotenv"));
-// import expressEjsLayouts from 'express-ejs-layouts';
+var express_ejs_layouts_1 = __importDefault(require("express-ejs-layouts"));
 var cookie_parser_1 = __importDefault(require("cookie-parser"));
-dotenv_1.default.config();
+// env.config();
 var app = (0, express_1.default)();
 app.use(express_1.default.static('public'));
-// app.set('view engine', 'ejs');
-// app.use(expressEjsLayouts);
+app.set('view engine', 'ejs');
 app.use((0, cookie_parser_1.default)());
+app.use(express_ejs_layouts_1.default);
 app.use(express_1.default.urlencoded({ extended: true })); //For Login body json, POST METHOD
 // app.use(express.json()); //For Login body json, POST METHOD
-app.use((0, cors_1.default)({
-    origin: "https://uptdsdn2kalimati.vercel.app",
-    credentials: true
-}));
+app.use((0, cors_1.default)());
 app.use('/', router_1.default);
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,20 @@
 import express from 'express';
 import rounter from './router';
 import cors from 'cors';
-import env from 'dotenv';
-// import expressEjsLayouts from 'express-ejs-layouts';
+import expressEjsLayouts from 'express-ejs-layouts';
 import cookieParser from 'cookie-parser';
-env.config();
+// env.config();
 
 const app = express();
 
 app.use(express.static('public'));
-// app.set('view engine', 'ejs');
-// app.use(expressEjsLayouts);
+app.set('view engine', 'ejs');
 app.use(cookieParser());
+app.use(expressEjsLayouts);
 app.use(express.urlencoded({ extended: true })); //For Login body json, POST METHOD
 // app.use(express.json()); //For Login body json, POST METHOD
-app.use(cors({
-  origin: `https://uptdsdn2kalimati.vercel.app`,
-  credentials: true
-}));
+app.use(cors());
 
-app.use('/',rounter);
+app.use('/', rounter);
 
 export default app;
