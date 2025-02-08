d025dd372e3392f3f8c45516d9071c8c831a5595
aldilukmn <aldilukman99@gmail.com>
2025-02-08 19:14:41 +0700

    add: cors url

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(expressEjsLayouts);
 app.use(express.urlencoded({ extended: true })); //For Login body json, POST METHOD
 // app.use(express.json()); //For Login body json, POST METHOD
 app.use(cors({
-  origin: ['https://uptdsdn2kalimati.vercel.app', 'http://localhost:5173'], // Sesuaikan dengan origin frontend kamu
+  origin: ['https://uptdsdn2kalimati.vercel.app', 'http://localhost:5173', 'http://localhost:3000'], // Sesuaikan dengan origin frontend kamu
   methods: ['GET', 'POST', 'PATCH', 'DELETE'], // Sesuaikan dengan method yang kamu perlukan
   credentials: true
 }));
