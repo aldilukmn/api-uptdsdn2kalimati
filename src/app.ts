9cdf43da7ce0449044611e9108c11b5126036315
aldilukmn <aldilukman99@gmail.com>
2025-02-08 20:35:35 +0700

    fix: cors handle 6

--- src/app.ts
+++ src/app.ts
@@ -23,6 +23,8 @@ app.use((req: Request, res: Response, next: Next) => {
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
   };
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 })
